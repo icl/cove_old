d15e00bfb2985a64f785aedd651f25712d1c700a
Ryan Weald <ryan@weald.com>
2011-01-14 10:29:12 -0800

    refactor display_flash helper to output correct html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   def display_flash
     html = ""
     if flash[:alert]
-      html << "<div class='flash alert'><%= flash[:error] %> </div>"
+      html << "<div class='flash alert'> #{flash[:alert]}</div>"
     end
     if flash[:notice]
-      html << "<div class='flash notice'><%= flash[:notice] %> </div>"
+      html << "<div class='flash notice'>#{flash[:notice]}</div>"
     end
-    return html
+    return raw(html)
   end
 end
