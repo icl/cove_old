783a4c157bc6db6c81e02b02771ae675e48f7b92
Ryan Weald <ryan@weald.com>
2011-01-13 21:35:31 -0800

    fix bug in feature syntax and create the step stubs

--- features/authenticate_user.feature
+++ features/authenticate_user.feature
@@ -4,12 +4,12 @@ Feature: Authenticate user
   I want to submit my user name and password and login to the system.
   
   
-  Scenario Outline: Valid Login
+  Scenario: Valid Login
     Given a regular_user exists
     When the user enters the correct email and password
     Then they should be successfully logged in to the system
     
-  Scenario Outline: Invalid Login credentials
+  Scenario: Invalid Login credentials
     Given a regular_user exists
     When they user enters an incorrect email or password
     Then the user should not be logged in and should be redirected back to the login form
---
+++ features/step_definitions/authenticate_user_steps.rb
@@ -0,0 +1,19 @@
+Given /^a regular_user exists$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the user enters the correct email and password$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^they should be successfully logged in to the system$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^they user enters an incorrect email or password$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the user should not be logged in and should be redirected back to the login form$/ do
+  pending # express the regexp above with the code you wish you had
+end
