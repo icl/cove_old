2fbb12726b5a407b0b18cc64ff6a25bec478fc4c
Ryan Weald <ryan@weald.com>
2011-01-19 15:52:58 -0800

    add additional scenario for user invitations. tests the user accepting the invitation.

--- features/step_definitions/invite_user_steps.rb
+++ features/step_definitions/invite_user_steps.rb
@@ -21,10 +21,31 @@ When /^they fill in the email address and submit$/ do
 end
 
 Then /^there should be a new user in the db$/ do
-  User.where(:email => "invite@devise.com").lenght.should == 1
+  User.where(:email => "invite@devise.com").length.should == 1
 end
 
-Then /^the user should receive an email$/ do
+# Then /^the user should receive an email$/ do
+#   pending # express the regexp above with the code you wish you had
+# end
+
+
+Given /^a user has received an invitation$/ do
+  @user = User.invite_user! :email => "invite@devise.com"
+end
+
+When /^the user visits the invitation acceptance page$/ do
+  visit url_for(:controller => "invitations", :action => "edit", :id => @user.invitation_token)
+end
+
+When /^the user fills in their new password$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the user should be redirected to root$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the user should be loggedin$/ do
   pending # express the regexp above with the code you wish you had
 end
 
