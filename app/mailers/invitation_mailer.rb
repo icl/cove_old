67b5b82b5d20e02a6118753a4d81f8e3a4590b8c
Ryan Weald <ryan@weald.com>
2011-01-20 17:24:28 -0800

    add the views for the invitation mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -2,7 +2,7 @@ class InvitationMailer < ActionMailer::Base
   default :from => "from@example.com"
   def welcome_email(user)
     @user = user
-    @url  = "localhost:3000/"
+    @url  = url_for (:controller => "invitations", :action => "edit", :id => @user.invitation_token)
     mail(:to => @user.email,
          :subject => "Welcome to my Site")
   end
