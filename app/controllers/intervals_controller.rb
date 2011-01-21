d21659b92c954e97830b5f953373b0323a007660
adodge <alexdodge@gmail.com>
2011-01-21 14:24:19 -0800

    Trying some new interface ideas.

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -3,17 +3,29 @@ class IntervalsController < ApplicationController
   # GET /intervals.xml
   def index
     intervals = Interval.find(:all, :order => 'start_time').map{|int|
+    	# Derive a "duration" field from the start and end times.
 	duration = (int.end_time - int.start_time)
 	hours = (duration / (60*60)).floor
 	minutes = ((duration - hours*60*60)/60).floor
 	int[:duration] = sprintf("%02dh%02dm", hours, minutes);
 	int
+    }.map{|int|
+	# Derive a "day" field from the start time.
+	int[:day] = sprintf("%d-%d-%d", int.start_time.month, int.start_time.day, int.start_time.year)
+	int
+    }.map{|int|
+	# Derive a "start_time_of_day" field from the start time
+	int[:start_time_of_day] = sprintf("%02d:%02d %s", int.start_time.hour % 12, int.start_time.min, (int.start_time.hour / 12).floor == 0 ? "AM" : "PM")
+	int
     }
+
+    # Build arrays of the unique angles and days.
     @angles = intervals.map {|x| x.camera_angle}.uniq.sort
     @days = intervals.map{|x| x.start_time}.map {|x|
 	sprintf("%d-%d-%d", x.month, x.day, x.year)
     }.uniq
 
+    # Filter by the passed parameters
     @filtered_intervals = intervals.reject{|x|
     	if(params[:date].nil? || params[:date] == "")
 		false
