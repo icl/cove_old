32de8c72229a77161f033043eab0e4d85883592b
Ryan Weald <ryan@weald.com>
2011-01-20 17:40:39 -0800

    fix bug with invitation email urls

--- spec/mailers/invitation_mailer_spec.rb
+++ spec/mailers/invitation_mailer_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe InvitationMailer do
   describe "#welcome_email" do
     before(:each) do
-      InvitationMailer.welcome_email(Factory(:first_time_user)).deliver
+      InvitationMailer.welcome_email(Factory(:invited_user)).deliver
     end
     it "should have sent an email" do
       # @email.deliver
