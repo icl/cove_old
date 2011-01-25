88b551a3ca50312074bb9507c8657dc45c515569
Ryan Weald <ryan@weald.com>
2011-01-25 14:46:21 -0800

    finish admins ability to add new users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,4 @@
+require "ruby-debug"
 class Admin::UsersController < ApplicationController
   before_filter :require_admin
   before_filter :authenticate_user!
@@ -7,6 +8,29 @@ class Admin::UsersController < ApplicationController
   end
   
   def new
+    @error_list = session[:error_list]
+    session[:error_list] = nil
     render 'new'
+  end
+  
+  def create
+    @error_list = []
+    if params[:user][:email_list]
+      email_string = params[:user][:email_list].read
+      email_list = email_string.split(",")
+      email_list.each do |email|
+        if !User.invite_user!(:email => email.strip)
+          @error_list << email.strip
+        end
+      end
+    elsif (email = params[:user][:email])
+      if !User.invite_user!(:email => email)
+        @error_list << email.strip
+      end
+    else
+      flash[:alert] = "The file or email you entered was not valid please try again"
+    end
+    session[:error_list] = @error_list
+    redirect_to :action => "new"
   end
 end
