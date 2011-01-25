a1f6b801ce629536128bbe30d307ac2fa4272e33
Ryan Weald <ryan@weald.com>
2011-01-25 12:56:06 -0800

    refactor admin user controller spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe Admin::UsersController do
+  before(:each) {sign_in Factory(:admin_user)}
   describe "GET 'index'" do
-    before(:each) do
-      sign_in Factory(:admin_user)
-      get 'index'
-    end
+    before(:each) {get 'index'}
     it {should respond_with(:success)}
     it {should render_template(:index)}
+  end
+  
+  describe "GET 'new'" do
+    before(:each){get 'new'}
   end
 end
