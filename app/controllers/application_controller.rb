af6781cd4caf4f5848243ef2751025ad8584570a
Ryan Weald <ryan@weald.com>
2011-01-25 12:46:07 -0800

    change flash message on require_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   
   def require_admin
     if !current_user || !current_user.admin
-      flash[:alert] = "You must be an admin to access this feature"
+      flash[:alert] = "You must be loggedin as an admin to access this feature"
       return redirect_to(root_path)
     end
   end
