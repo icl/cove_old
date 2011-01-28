fcb0c669e24c7b90fa5a55a4f282c1ed9ed2860b
Paul Panarese <git@panjunction.com>
2011-01-27 16:56:37 -0800

    Changed all tests to use the first person when needed

--- features/step_definitions/authenticate_user_steps.rb
+++ features/step_definitions/authenticate_user_steps.rb
@@ -1,8 +1,8 @@
-Given /^a regular_user exists$/ do
+Given /^(?:|I )am a regular user$/ do
   Factory(:regular_user)
 end
 
-Given /^a regular_user is logged_in$/ do
+Given /^(?:|I )am a regular user who is logged in$/ do
   Factory(:regular_user)
   visit("/login")
   fill_in("user_email", :with => "user@test.com")
@@ -10,20 +10,20 @@ Given /^a regular_user is logged_in$/ do
   click_button("user_submit")
 end
 
-When /^the user enters the correct email and password$/ do
+When /^(?:|I )enter the correct email and password$/ do
   visit("/login")
   fill_in("user_email", :with => "user@test.com")
   fill_in("user_password", :with => "myPassword")
   click_button("user_submit")
 end
 
-When /^the user enters an incorrect email or password$/ do
+When /^(?:|I )enter an incorrect email or password$/ do
   visit("/login")
   fill_in("user_email", :with => "bad@value.com")
   fill_in("user_password", :with => "foobar")
   click_button("user_submit")
 end
 
-Given /^there is nobody logged_in$/ do
+Given /^there is nobody logged in$/ do
   visit("/logout")
 end
--- features/step_definitions/require_nda_steps.rb
+++ features/step_definitions/require_nda_steps.rb
@@ -1,8 +1,8 @@
-Given /^a user who has not yet signed the NDA$/ do
+Given /^(?:|I )have not yet signed the NDA$/ do
   Factory(:first_time_user)
 end
 
-When /^the user logs in to the site for the first time$/ do
+When /^(?:|I )login to the site for the first time$/ do
   visit("/login")
   fill_in("user_email", :with => "first@time.com")
   fill_in("user_password", :with => "password")
