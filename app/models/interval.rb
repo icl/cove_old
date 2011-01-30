d71c68e387c9fd63faeb61e2a3eb54ab136ff871
Joshua Sonnikson <jsonnikson@gmail.com>
2011-01-29 17:37:46 -0800

    many-to-many relationship btw Interval and Tag through IntervalTags

--- app/models/interval.rb
+++ app/models/interval.rb
@@ -1,2 +1,4 @@
 class Interval < ActiveRecord::Base
+  has_many :interval_tags
+  has_many :tags, :through => :interval_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ActiveRecord::Base
+  has_many :interval_tags
+  has_many :intervals, :through => :interval_tags
 end
