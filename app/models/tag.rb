574339f735afb43e7e49e8c839bf2d68b7ca4368
Joshua Sonnikson <jsonnikson@gmail.com>
2011-01-29 22:17:42 -0800

    validate tag name is unique

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
   has_many :interval_tags
-  has_many :intervals, :through => :interval_tags
+  has_many :intervals, :through => :interval_tags 
+  validates_uniqueness_of :name
 end
