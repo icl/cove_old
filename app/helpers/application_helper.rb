3b4243a2bcadbd721864e4b8ab607a6f0c569596
omnibruno <tscott@ucsd.edu>
2011-02-01 14:48:02 -0800

    fixed nda container. Added grid for flash alert and notices as well as styled them for color (red and green respectively).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def display_flash
     html = ""
     if flash[:alert]
-      html << "<div class='flash alert'>#{flash[:alert]}</div>"
+      html << %[<div class="grid_16 flash alert">#{flash[:alert]}</div>]
     end
     if flash[:notice]
-      html << "<div class='flash notice'>#{flash[:notice]}</div>"
+      html << %[<div class="grid_16 flash notice">#{flash[:notice]}</div>]
     end
     return raw(html)
   end
