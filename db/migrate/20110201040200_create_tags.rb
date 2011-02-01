6852f33d69d80f06849be8c6abb88e1718bcc145
Ryan Weald <ryan@weald.com>
2011-02-01 15:27:11 -0800

    remove the bad columns from tags table. Also add the necessary reference id columns

--- db/migrate/20110201040200_create_tags.rb
+++ db/migrate/20110201040200_create_tags.rb
@@ -1,9 +1,10 @@
 class CreateTags < ActiveRecord::Migration
   def self.up
     create_table :tags do |t|
-      t.datetime :start_time
-      t.datetime :end_time
       t.string :name
+      t.integer :user_id
+      t.integer :interval_id
+      t.integer :tag_type_id
       t.timestamps
     end
   end
