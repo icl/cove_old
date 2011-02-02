620c52d48011933bdc8ec8c7a6740282816a6188
Ryan Weald <ryan@weald.com>
2011-02-01 16:47:11 -0800

    refactor the code in the project to make it cleaner

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,84 +1,50 @@
 class ProjectsController < ApplicationController
   before_filter :authenticate_user!
-  
-   def new 
-      #render :text => "In show"
-      @project = Project.new   
 
-	  @project.user = current_user
- 
-      respond_to do |format|
-         format.html # new.html.erb
-         format.xml  { render :xml => @project }
-      end
-   end
-
-  def index 
-      if user_signed_in?
-         @project = Project.all
-      end
+  def index
+    @project = Project.all
+    render "index"
   end
 
-  # GET /project/1
-  # GET /project/1.xml
+  def new
+    #render :text => "In show"
+    @project = Project.new
+    @project.user = current_user
+    render "new"
+  end
+
   def show
     @project = Project.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @project }
-    end
+    render "show"
   end
 
 
-  # POST /projects
-  # POST /projects.xml
   def create
     @project = Project.new(params[:project])
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to(@project, :notice => 'Project was successfully created.') }
-        format.xml  { render :xml => @project, :status => :created, :location => @project }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
-      end
+    if @project.save
+      redirect_to(@project, :notice => 'Project was successfully created.')
+    else
+      render "new"
     end
   end
 
-  # GET /project/1/edit
   def edit
     @project = Project.find(params[:id])
-
+    render "edit"
   end
 
-  # DELETE /project/1
-  # DELETE /project/1.xml
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(@project, :notice => 'Project was successfully deleted.') }
-      format.xml  { head :ok }
-    end
+    redirect_to(@project, :notice => 'Project was successfully deleted.')
   end
 
-  # PUT /intervals/1
-  # PUT /intervals/1.xml
   def update
     @project = Project.find(params[:id])
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to(@project, :notice => 'Project was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @interval.errors, :status => :unprocessable_entity }
-      end
+    if @project.update_attributes(params[:project])
+      redirect_to(@project, :notice => 'Project was successfully updated.')
+    else
+      render "edit"
     end
   end
-
 end
