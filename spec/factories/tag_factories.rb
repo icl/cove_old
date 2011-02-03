7b5a55a5f7372ad25738a19112323b3f35011907
Ryan Weald <ryan@weald.com>
2011-02-03 10:13:26 -0800

    modify the taging factory more to not have user exists conflict

--- spec/factories/tag_factories.rb
+++ spec/factories/tag_factories.rb
@@ -3,7 +3,13 @@ Factory.define :tag, :class => Tag do |f|
 end
 
 Factory.define :taging, :class => Taging do |f|
-  f.association :user, :factory => :regular_user
+  f.user do
+    if User.where(:email => "user@test.com").first
+      User.where(:email => "user@test.com").first
+    else
+      Factory(:regular_user)
+    end
+  end
   f.association :interval, :factory => :interval
   f.association :tag, :factory => :tag
 end
