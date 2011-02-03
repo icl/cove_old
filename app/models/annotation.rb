2e8608af342846e0f36c2aa9ea80a7060a69db5d
Ryan Weald <ryan@weald.com>
2011-02-03 10:24:51 -0800

    refactor annotations class. class methods and instance methods are now grouped

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -28,6 +28,47 @@ class Annotation
     return result
   end
   
+  def self.add(args)
+    type = args.delete(:type)
+    name = args.delete(:name)
+    resource = Annotation.class_from_symbol(type).where(:name => name).first
+
+    # handle a bad name for the actual model class
+    unless resource
+      if Annotation.class_from_symbol(type).can_be_created?
+        unless Annotation.class_from_symbol(type).create(:name => name)
+          return false
+        end
+      else
+        return false
+      end
+    end
+
+    # create the new join model instance.
+    new_object = Annotation.join_class_from_symbol(type).new(args)
+    new_object.send "#{type.to_s}=", resource
+    new_object.user = args[:user]
+    new_object.interval = args[:interval]
+    return new_object.save
+  end
+  
+  def self.join_class_from_symbol(symbol)
+    if @@types_of_annotation.include? symbol
+      symbol.to_s.concat("ing").capitalize.constantize
+    else
+      raise Exception
+    end
+  end
+
+  def self.class_from_symbol(symbol)
+    if @@types_of_annotation.include? symbol
+      symbol.to_s.capitalize.constantize
+    else
+      raise Exception
+    end
+  end
+  
+  
   
   # DEPRECATED METHOD YOU SHOULD USE THE CLASS METHOD INSTEAD
   # def add(args)
@@ -60,37 +101,14 @@ class Annotation
 
   # class version of the add method. Needs a user_id, interval_id
   # annotation type, name, and any additional arguments
-  def self.add(args)
-    type = args.delete(:type)
-    name = args.delete(:name)
-    resource = Annotation.class_from_symbol(type).where(:name => name).first
-
-    # handle a bad name for the actual model class
-    unless resource
-      if Annotation.class_from_symbol(type).can_be_created?
-        unless Annotation.class_from_symbol(type).create(:name => name)
-          return false
-        end
-      else
-        return false
-      end
-    end
-
-    # create the new join model instance.
-    new_object = Annotation.join_class_from_symbol(type).new(args)
-    new_object.send "#{type.to_s}=", resource
-    new_object.user = args[:user]
-    new_object.interval = args[:interval]
-    return new_object.save
-  end
 
 
-  def annotations_for_user(resource_class)
-    resource_class.where(:user_id => self.user_id)
+  def annotations_for_user(join_class, resource_class)
+    join_class.joins(resource_class.to_s.downcase.to_sym).where(:user_id => @user_id)
   end
 
   def annotations_for_interval(resource_class)
-    resource_class.where(:interval_id => self.interval_id)
+    join_class.joins(resource_class.to_s.downcase.to_sym).where(:interval_id => @interval_id)
   end
 
   def where(args)
@@ -102,20 +120,5 @@ class Annotation
   end
 
   
-  def self.join_class_from_symbol(symbol)
-    if @@types_of_annotation.include? symbol
-      symbol.to_s.concat("ing").capitalize.constantize
-    else
-      raise Exception
-    end
-  end
-
-  def self.class_from_symbol(symbol)
-    if @@types_of_annotation.include? symbol
-      symbol.to_s.capitalize.constantize
-    else
-      raise Exception
-    end
-  end
 
 end
