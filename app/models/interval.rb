57e6ccdfda935be8e64f605312d908a4afe4d4da
Paul Panarese <git@panjunction.com>
2011-02-05 13:22:18 -0800

    Added CSV import skeleton to interval model

--- app/models/interval.rb
+++ app/models/interval.rb
@@ -1,2 +1,50 @@
 class Interval < ActiveRecord::Base
+  
+  def import
+    Dir.foreach("tmp/notes/") do |file|
+      if file =~ /[^(\.|\.\.)].*csv$/
+        note_file = File.new("tmp/notes/#{file}")
+        notes = FasterCSV.new(note_file,
+          :headers => true,
+          :header_converters => :symbol,
+          :skip_blanks => true;
+          :col_sep => ','
+        )
+        notes.convert do |field, info|
+          case info.header.underscore
+            when :file_name
+              
+            when :camera_angle
+              
+            when :session_number
+              
+            when :start_time
+              
+            when :end_time
+              
+            when :session_type
+              
+            when :phrase_name
+              
+            when :phrase_type
+              
+            when :task_name
+              
+            when :comments
+              
+            else
+              
+          end # End |case| block
+        end # End |do| block
+        
+        report.each do |row|
+          Interval.create(row.to_hash)
+        end
+        
+        File.move("tmp/notes/#{file}","log/notes/#{file}.imported_at_#{Time.now.strftime("%Y%m%d%H%M")}")
+        
+      end # End |if file| block
+    end # End |Dir.foreach| block
+  end # End |def import| block
+  
 end
