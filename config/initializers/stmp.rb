7fd93c6ec41c3268f2e77f26b0ff1d63aa147a87
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-02-06 21:31:21 -0800

    added mailer config

---
+++ config/initializers/stmp.rb
@@ -0,0 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com"
+}
+
+class DevelopmentMailInterceptor
+  def self.delivering_email(message)
+    message.subject = "#{message.to} #{message.subject}"
+    message.to = "ethan.soutar.rau@gmail.com"
+  end
+end
+
+ActionMailer::Base.default_url_options[:host] = "cove.ucsd.edu"
+Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
