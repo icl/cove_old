b7df2f77c8c8d0d88f788a36f8547315788266cd
adodge <alexdodge@gmail.com>
2011-02-15 15:13:19 -0800

    Fade looks better than the resizing thing, to me.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 			var name = $(this).text();
 			hoverstatus[name] = 1;
 			if($("#definition_"+name).length){
-				$("#definition_"+name).show("fast");
+				$("#definition_"+name).fadeIn("fast");
 			}else{
 				$.get(
 					"/definitions/"+name,
@@ -15,7 +15,7 @@ $(document).ready(function(){
 						var def = $(data).find(".definition_holder").first().prependTo("body").hide().attr("id", "definition_"+name)
 						if(hoverstatus[name]){
 							def
-								.show("fast")
+								.fadeIn("fast")
 								.css("position", "absolute")
 								.css("top", (e.pageY + offY) + "px")
 								.css("left", (e.pageX + offX) + "px");
@@ -36,6 +36,6 @@ $(document).ready(function(){
 			$("#definition_"+name).trigger("goaway");
 		});
 	$(".definition_holder").live("goaway",function(){
-		$(this).hide("slow");
+		$(this).fadeOut("slow");
 	});
 });
