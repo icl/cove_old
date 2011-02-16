56482abcc972da3732fac7be8da2336569d86bf4
Ryan Weald <ryan@weald.com>
2011-02-16 11:11:21 -0800

    add create method to to annotations controller

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -6,9 +6,20 @@ class AnnotationsController < ApplicationController
     respond_with(@annotations)
   end
 
-
+  def create
+    if new_annotation = Annotation.add(:type => params[:type], 
+                    :invterval => @interval, 
+                   :user => current_user, :name => params[:name],
+                     :opt => params[:opt])
+      flash[:notice] = "You have successfully created a new annotation"
+    end
+    respond_with do |format|
+      format.html {redirect_to :action => "index"}
+      format.json { render :json => new_annotation}
+    end
+  end
+  private
   def find_interval
     @interval = Interval.find(params[:interval_id])
   end
-  private :find_interval
 end
--- spec/controllers/annotations_controller_spec.rb
+++ spec/controllers/annotations_controller_spec.rb
@@ -5,16 +5,26 @@ describe AnnotationsController do
     @taging = Factory(:taging)
   end
   describe "GET 'index'" do
-    context "format json" do
-      it "should return all annotations" do
-        perform_get(:json)
-        response.body.should == Annotation.all(:tag).to_json
-      end
+    it "should return all annotations" do
+      perform_get(:json)
+      response.body.should == Annotation.all(:tag).to_json
     end
+  end
+
+  describe "POST 'create'" do
+    it "should create a new taging in the db" do
+      old_count = Annotation.all(:tag).length
+      perform_post
+      Annotation.all(:tag).length.should be > old_count
+    end
+  end
+
+  def perform_post
+    post :create, :format => :json, :interval_id => @taging.interval_id, 
+      :type => :tag, :name => "Test"
   end
   def perform_get(format_type)
     get :index, :format => format_type,
       :interval_id => @taging.interval_id, :type => :tag
-    
   end
 end
