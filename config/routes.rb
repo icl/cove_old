f23dc155a46eaface4c89ac8d510cc2f4c16c5ea
Paul Panarese <git@panjunction.com>
2011-02-17 14:18:24 -0800

    Added routes for favorites and queue for user and projects and fixed some titles

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,29 @@ Cove::Application.routes.draw do
   resources :projects
   
   resources :collections
+  # Collections General Routes
   match 'collections/:id/add/:interval' => 'collections#add', :as => :add_to_collection
   match 'collections/:id/remove/:interval' => 'collections#remove', :as => :remove_from_collection
   match 'collections/:id/prioritize/:interval/:direction' => 'collections#prioritize_interval', :as => :prioritize_interval
   match 'collections/:id/prioritize' => 'collections#prioritize_intervals', :as => :prioritize_intervals
-  #match 'projects/:id/add/:collection' => 'projects#add_collection', :as => :add_collection
-  #match 'projects/:id/remove/:collection' => 'projects#remove_collection', :as => :remove_collection
+  # Project specific Collection Routes
+  match 'projects/:project_id/add/:collection' => 'projects#add_collection', :as => :add_collection
+  match 'projects/:project_id/remove/:collection' => 'projects#remove_collection', :as => :remove_collection
+  match 'projects/:project_id/favorites' => 'collections#show', :as => :favorite_intervals
+  match 'projects/:project_id/favorites/add/:interval' => 'collections#add', :as => :add_to_project_favorites
+  match 'projects/:project_id/favorites/remove/:interval' => 'collections#remove', :as => :remove_from_project_favorites
+  match 'projects/:project_id/queue' => 'collections#show', :as => :interval_queue
+  match 'projects/:project_id/queue/add/:interval' => 'collections#add', :as => :add_to_project_queue
+  match 'projects/:project_id/queue/remove/:interval' => 'collections#remove', :as => :remove_from_project_quese
+
+  # User specific Collection Routes
+  match 'users/:user_id/favorites' => 'collections#show', :as => :user_favorites
+  match 'users/:user_id/favorites/add/:interval' => 'collections#add', :as => :add_to_user_favorites
+  match 'users/:user_id/favorites/remove/:interval' => 'collections#remove', :as => :remove_from_user_favorites
+  match 'users/:user_id/queue' => 'collections#show', :as => :user_queue
+  match 'users/:user_id/queue/add/:interval' => 'collections#add', :as => :add_to_user_queue
+  match 'users/:user_id/queue/remove/:interval' => 'collections#remove', :as => :remove_from_user_queue
+
 
   devise_for :users, :path => "/", :path_names => {:sign_in => "login", 
     :sign_out => "logout"}
