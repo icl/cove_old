f01656c61fe52884942bfc605e75615c9f585765
jinnismi <jin.jinny.jin.jin@gmail.com>
2011-02-17 10:19:13 -0800

    done with html and css for show but have js in html atm

---
+++ public/javascripts/application.js
@@ -0,0 +1,33 @@
+
+
+
+
+/***********************************/
+/* JS adjustments for show.html */
+
+
+$(document).ready(function(){
+	$(".show_tags p").click(function(){
+		$("show_tags ul").slideToggle("slow");
+	});
+});
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* end style adjustments for show.html */
+/***************************************/
