034404c938373203d47169fb0f3f7b355e795211
Ryan Weald <ryan@weald.com>
2011-02-17 18:06:44 -0800

    create method the created underlying tag when tagging created Also remove the old taging model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,5 @@
 class Tag < ActiveRecord::Base
   validates_uniqueness_of :name
   has_many :taggings, :dependent => :delete_all
-  
-  def self.can_be_created?
-    true
-  end
-  
+   
 end
--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,9 +1,29 @@
+require "ruby-debug"
 require 'spec_helper'
 
 describe Tagging do
+  before(:each) do
+    @user = Factory(:regular_user)
+    @interval = Factory(:interval)
+  end
   describe "validation" do
-    it {should validate_presence_of(:tag_id)}
     it {should validate_presence_of(:user_id)}
     it {should validate_presence_of(:interval_id)}
+  end
+
+  describe "#hookup_tag" do
+    before(:each) do
+      #@tag = Factory(:tag)
+      @result = Tagging.create :name => "NewTest", :user => @user, :interval => @interval  
+    end
+    context "valid tag info" do
+      it {@result.should be_true}
+      it "should link it to an underlying tag" do
+        @result.tag.should be
+      end
+      it "should create an underlying tag" do
+        Tag.where(:name => "NewTest").length().should be > 0
+      end
+    end
   end
 end
