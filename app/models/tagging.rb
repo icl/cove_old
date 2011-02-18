f3869c537a554d1c611f32ba9b3c5fc314b8d5ad
Ryan Weald <ryan@weald.com>
2011-02-17 17:26:14 -0800

    more testing!!!

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -2,4 +2,8 @@ class Tagging < ActiveRecord::Base
   belongs_to :tag
   belongs_to :user
   belongs_to :interval
+
+  validates_presence_of :interval_id
+  validates_presence_of :user_id
+  validates_presence_of :tag_id
 end
--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Tagging do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    it {should validate_presence_of(:tag_id)}
+    it {should validate_presence_of(:user_id)}
+    it {should validate_presence_of(:interval_id)}
+  end
 end
