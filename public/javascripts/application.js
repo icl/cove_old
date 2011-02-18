3f9f963900cd202570760f0c73b3f9a3e6e6a9b3
adodge <alexdodge@gmail.com>
2011-02-18 01:06:03 -0800

    A new test which uses selenium.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 	var offY = 10;
 	var zindex_inc = 1000;
 	$(".define_me")
-		.live("mouseenter", function(e){
+		.live("mouseenter lookup", function(e){
+			$(this).css("background-color", "red");
 			var name = $(this).text();
 			hoverstatus[name] = 1;
 			if($("#definition_"+name.replace(" ", "_")).length){
