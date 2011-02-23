46263e2dbe4e4563a981b2b7b664f33858440320
adodge <alexdodge@gmail.com>
2011-02-22 17:19:58 -0800

    Tooltips are relative to the position of the element, not the cursor.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 	var hoverstatus = [];
-	var offX = 10;
-	var offY = 10;
+	var offX = 4;
+	var offY = 0;
 	var zindex_inc = 1000;
 	$(".define_me")
 		.live("mouseenter lookup", function(e){
 			var name = $(this).text();
+			var offset = $(this).offset();
+			var width = $(this).width();
 			hoverstatus[name] = 1;
 			if($("#definition_"+name.replace(" ", "_")).length){
 				$("#definition_"+name.replace(" ", "_")).fadeIn("fast");
@@ -24,8 +26,8 @@ $(document).ready(function(){
 							def
 								.fadeIn("fast")
 								.css("position", "absolute")
-								.css("top", (e.pageY + offY) + "px")
-								.css("left", (e.pageX + offX) + "px");
+								.css("top", (offset.top + offY) + "px")
+								.css("left", (offset.left + width + offX) + "px");
 						}
 					}
 				);
@@ -33,10 +35,12 @@ $(document).ready(function(){
 		})
 		.live("mousemove", function(e){
 			var name = $(this).text();
+			var offset = $(this).offset();
+			var width = $(this).width();
 			$("#definition_"+name.replace(" ", "_"))
 				.css("position", "absolute")
-				.css("top", (e.pageY + offY) + "px")
-				.css("left", (e.pageX + offX) + "px");
+				.css("top", (offset.top + offY) + "px")
+				.css("left", (offset.left + width + offX) + "px");
 		})
 		.live("mouseleave", function(){
 			var name = $(this).text();
