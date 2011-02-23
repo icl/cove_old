5d0bc1cdd4b70b4011d3b17ead9110c68fdd859a
PratikPramanik <ppramaniucsd@gmail.com>
2011-02-23 14:52:53 -0800

    Code Importation working so far

--- app/models/code.rb
+++ app/models/code.rb
@@ -14,4 +14,60 @@ class Code < ActiveRecord::Base
     #side terms
     text :coding_type
   end
+
+=begin
+  modding code from Paul Panarease's interval import. Pratik Commented it
+=end
+  def self.import!
+    Dir.foreach("tmp/codings/") do |file|  #for each file in tmp definitions
+      
+      if file =~ /[^(\.|\.\.)].*csv$/  #check if its a CSV file
+        note_file = File.new("tmp/codings/#{file}") #save file in temp variable
+        notes = FasterCSV.new(note_file, #throw CSV file at FasterCSV tool
+          :headers => true,
+          :header_converters => [lambda {|h| h.gsub(/Name/, 'name').gsub(/Type/, 'type').gsub(/Description/, 'description')}, :symbol],
+          :skip_blanks => true,
+          :col_sep => ','
+        )
+
+        #converts notes to a usable format
+        notes.convert do |field, info|
+	        field.to_s
+        end # End |do| block
+        
+
+        #puts notes inside the db, NEEDS TO BE MODDED FOR PHENOMENON!
+        notes.each do |row|
+          def_type = row[:type]
+          #case def_type
+           # when "Phenomenon"
+              stored_phen = Code.where(:name => row[:name], :coding_type => row[:type]).first
+              if stored_phen == nil
+                new_phen = Code.new
+                new_phen.name = row[:name]
+                new_phen.coding_type = row[:type]
+                new_phen.description = row[:description]
+              	new_phen.save
+                puts "fuck 1",new_phen.name," ",new_phen.coding_type," ",new_phen.description
+              else
+                stored_phen.description = row[:description]
+              	stored_phen.save
+                puts "fuck 2"
+              end
+            #else
+            #  puts "Unrecognized type. create new tag type then try again."
+         # end #end case
+        end #end |do row|
+
+
+        if !File.directory?('log/codings') # Doesn't work for some reason. Directory needs to be created manually
+          Dir.mkdir('log/codings')
+        end
+        
+        #move file to new store
+        File.move("tmp/codings/#{file}","log/codings/#{file}.imported_at_#{Time.now.strftime("%Y%m%d%H%M")}")
+        
+    end # End |Dir.foreach| block
+  end # End |def import| block
+end
 end
