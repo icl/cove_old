227b8c2a01bbc425ea285e0bd564acbdda6cca42
adodge <alexdodge@gmail.com>
2011-02-24 18:36:20 -0800

    Attempting to get solr working with cucumber

--- features/step_definitions/fauxselect_steps.rb
+++ features/step_definitions/fauxselect_steps.rb
@@ -8,3 +8,16 @@ When /^(?:|I )select "([^"]+)" from the fauxselect "([^"]*)"$/ do |selection, se
 		click_link(selection)
 	end
 end
+
+When /^(?:|I )filter by the camera angle "([^"]*)"$/ do |selection|
+	page.driver.browser.execute_script %Q{ $(".camera_angle_selector").find(".fauxselect_button").trigger("openme"); }
+	with_scope(".camera_angle_selector") do
+		click_link(selection)
+	end
+end
+When /^(?:|I )filter by the phrase type "([^"]*)"$/ do |selection|
+	page.driver.browser.execute_script %Q{ $(".phrase_type_selector").find(".fauxselect_button").trigger("openme"); }
+	with_scope(".phrase_type_selector") do
+		click_link(selection)
+	end
+end
