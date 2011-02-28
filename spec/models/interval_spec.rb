3fa6380138c7bb8fecfb6a0a697324fe709c11ed
adodge <alexdodge@gmail.com>
2011-02-27 19:55:13 -0800

    Beginnings of testing.  Sort of shit.

--- spec/models/interval_spec.rb
+++ spec/models/interval_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
 
 describe Interval do
+	describe "when searched" do
+		before(:each) do
+			testIntervals = [
+				{
+					:camera_angle => "aaa",
+				},
+				{
+					:camera_angle => "aa",
+				},
+				{
+					:camera_angle => "a",
+				},
+			]
+			testIntervals.each { | properties |
+				Interval.new(properties).save()
+			}
+		end
+		it "returns substring matches for searches" do
+			result = Interval.search(:query => "a")
+			result.length.should == 3
+		end
+		it "returns exact matches for filters" do
+			result = Interval.search(:camera_angle => "a")
+			result.length.should == 1
+		end
+	end
 end
