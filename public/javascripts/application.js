481fc0565377cfabf95a191247d85b827aa02189
savethewaves <chrislei@gmail.com>
2011-02-28 18:15:10 -0800

    removed old imagemap thumbnail javascript code and misc debug comments

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,33 +1,13 @@
-    function swapThumb(obj)
-    {
-        num=obj.id;
-        name=obj.title;
-        //alert("in swapThumb");
-        // Improvements to remove flickering issue
-        // 1) pre-buffer images and only show them when fetch is complete
-        // 2) fetch a sprite (ruby sprite gem or imagicmagik)
-        //alert("in swapThumb method")
-        document.getElementById("thumb_"+name).innerHTML='<img src ="/thumbs/'+num+'.jpg" width ="160" height ="90" usemap="#thumbmap_'+name+'" />';
-    }
-    
-    //document.getElementById("thumb_"+name).addEventListener('onmouseover',swapThumb, false) {}
-    //$('.thumb_scrub').live('mouseover', function(event) {
-       //alert("in bind fuction")
-       //swapThumb(this); 
-    //});
-    
+/* Thumbnail fast scrub */    
     function changeSpriteWindow(obj){
-        // DEBUG //console.warn($(obj).position().left);
         //TODO: figure out how to grab misc 10 pixel additional margin
         var extra_margin =10;
         var newY = (event.clientX-($(obj).position().left)-extra_margin)*90;
         var position = "0px " + newY + "px "
-        // DEBUG //console.warn(position);
         obj.style.backgroundPosition = position;
     }
     
     $('.thumbnail_box').live('mousemove', function(event) {
-        //console.warn(event.clientX);
         changeSpriteWindow(this);
     });
     
@@ -36,9 +16,9 @@
             this.style.backgroundImage="url(/thumbs/2010-10-27_Wayne_12-07-28_Session1_sprite.jpg)";
             $(this).addClass("thumb_loaded");
         }
-        //console.warn("entering thumbnail box handler");
         $(this).children("img").hide();
     });
+/* end Thumbnail fast scrub */    
 
 /***********************************/
 /* JS adjustments for show.html */
