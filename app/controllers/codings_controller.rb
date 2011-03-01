bcb297169018400d28830958cbecd7424138c0fb
Ryan Weald <ryan@weald.com>
2011-03-01 12:01:07 -0800

    we have passing specs for the codings_controller

---
+++ app/controllers/codings_controller.rb
@@ -0,0 +1,30 @@
+class CodingsController < ApplicationController
+  respond_to :html, :json
+  def create
+    @interval = Interval.find(params[:interval_id])
+    @new_coding = Coding.new(params[:coding])
+    @new_coding.interval = @interval
+    @new_coding.user = current_user
+    if @new_coding.save
+      respond_with do |format|
+        format.html {redirect_to interval_path(params[:interval_id])}
+        format.json do
+          render :json => {"status" => "success", 
+            "codeName" => @new_coding.code.name, "tagging" => @new_coding.to_json}, :status => 201
+        end
+      end
+    else
+      respond_with do |format|
+        format.html {redirect_to interval_path(params[:interval_id])}
+        format.json do
+          render :json => {"status" => "failure"}, :status => 422
+        end
+      end
+    end
+  end
+
+  def show
+    @code = Coding.find(params[:id])
+    respond_with(@code)  
+  end
+end
