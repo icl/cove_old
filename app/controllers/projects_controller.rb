350fbf94c377c34c9c4dfc8ab3f999f627e2ecb2
Paul Panarese <git@panjunction.com>
2011-03-01 08:40:27 -0800

    Removed deprecated columns from projects and updated CSS for all project and collection pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,9 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.all
+    if @projects.nil?
+      @project = Project.new
+    end
     render "index"
   end
 
