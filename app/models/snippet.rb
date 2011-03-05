5b22b5e0424327b5cbc59f5a06216bf70d01571c
Joshua Sonnikson <jsonnikson@gmail.com>
2011-03-05 15:32:05 -0800

    convert snippets to haml; write code to display times

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,19 @@
 class Snippet < ActiveRecord::Base
   validates_presence_of :offset, :duration
   belongs_to :interval
+
+  def offset_s
+    return time_to_string @offset
+  end
+
+  def end_time_s
+    return time_to_string @offset + @duration
+  end
+
+  def time_to_string(t)
+    deciseconds = (t / 100 % 100).round
+    seconds = (t % 60).floor
+    hours = (t / 60).floor
+    return "%02d:%02d.%02d" % [hours, seconds, deciseconds]
+  end
 end
