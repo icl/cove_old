a7ad0fa6d292c3858f40ed4dbedebf2ed3812143
Joshua Sonnikson <jsonnikson@gmail.com>
2011-03-12 13:34:02 -0800

    work on snippet model and form

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,18 +2,29 @@ class Snippet < ActiveRecord::Base
   validates_presence_of :offset, :duration
   belongs_to :interval
 
-  def offset_s
-    return time_to_string @offset
+  attr_accessor :start_time
+  attr_accessor :end_time
+
+  def start_time=(time_string)
+    offset = string_to_seconds time_string rescue nil
   end
 
-  def end_time_s
-    return time_to_string @offset + @duration
+  def start_time
+    seconds_to_string offset rescue nil
   end
 
-  def time_to_string(t)
-    deciseconds = (t / 100 % 100).round
+  def end_time=(time_string)
+    duration = (string_to_seconds time_string) - offset rescue nil
+  end
+
+  def end_time
+    seconds_to_string (offset + duration) rescue nil
+  end
+
+  def seconds_to_string(t)
+    centiseconds = (t / 100 % 100).round
     seconds = (t % 60).floor
-    hours = (t / 60).floor
-    return "%02d:%02d.%02d" % [hours, seconds, deciseconds]
+    minutes = (t / 60).floor
+    return "%02d:%02d.%02d" % [minutes, seconds, centiseconds]
   end
 end
